49cff58d2d2a699bb860811d850a4c621d72dcf8
Samuel Monroe <spat.monroe@gmail.com>
2022-03-28 09:33:28 +0200

    Chat is a live component

---
+++ lib/consensus_web/components/live_chat_component.ex
@@ -0,0 +1,51 @@
+defmodule ConsensusWeb.LiveChatComponent do
+  use ConsensusWeb, :live_component
+  alias Consensus.{Accounts, Message}
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form let={f} for={@changeset} phx-submit="save" phx-target={@myself}>
+        <%= label f, :body %>
+        <%= text_input f, :body %>
+        <%= error_tag f, :body %>
+
+        <%= submit "Send" %>
+      </.form>
+      <h1> Messages </h1>
+      <%= for message <- @messages do %>
+        <p><%= message.body %></p>
+      <% end %>
+    </div>
+    """
+  end
+
+  def update(assigns, socket) do
+    user_id = Accounts.get_user_by_session_token(assigns.user_token).id
+    messages = Message.get_all()
+    {:ok, assign(socket, %{messages: messages, user_id: user_id, changeset: Message.changeset(%Message{}, %{})})}
+  end
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def handle_event("save", %{"message" => message_params}, socket) do
+    case Message.create_message(Map.put(message_params, "user_id", socket.assigns.user_id)) do
+      {:ok, _message} ->
+        flash_parent(:info, "Message sent")
+        {:noreply, socket}
+      {:error, %Ecto.Changeset{} = changeset} ->
+        flash_parent(:error, "Invalid message")
+        {
+          :noreply,
+          socket
+          |> assign(changeset: changeset)
+        }
+    end
+  end
+
+  defp flash_parent(severity, message) do
+    send self(), {:flash, {severity, message}}
+  end
+end
--- lib/consensus_web/views/chat_live_view.ex
+++ lib/consensus_web/views/chat_live_view.ex
@@ -1,47 +1,25 @@
 defmodule ConsensusWeb.ChatLiveView do
   use ConsensusWeb, :live_view
-  alias Consensus.{Accounts, Message}
+  alias ConsensusWeb.LiveChatComponent
 
   def render(assigns) do
     ~H"""
-    <.form let={f} for={@changeset} phx-submit="save">
-      <%= label f, :body %>
-      <%= text_input f, :body %>
-      <%= error_tag f, :body %>
-
-      <%= submit "Send" %>
-    </.form>
-    <h1> Messages </h1>
-    <%= for message <- @messages do %>
-      <p><%= message.body %></p>
-    <% end %>
+    <.live_component module={LiveChatComponent} id="hero" user_token={@user_token} />
     """
   end
 
   def mount(_params, %{"user_token" => user_token}, socket) do
-    user_id = Accounts.get_user_by_session_token(user_token).id
     Phoenix.PubSub.subscribe Consensus.PubSub, "messages"
-    messages = Message.get_all()
-    {:ok, assign(socket, %{messages: messages, user_id: user_id, changeset: Message.changeset(%Message{}, %{})})}
-  end
-
-  def handle_event("save", %{"message" => message_params}, socket) do
-    case Message.create_message(Map.put(message_params, "user_id", socket.assigns.user_id)) do
-      {:ok, _message} ->
-        {:noreply, put_flash(socket, :info, "Message sent")}
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {
-          :noreply,
-          socket
-          |> put_flash(:error, "Invalid message")
-          |> assign(changeset: changeset)
-        }
-    end
+    {:ok, assign(socket, %{user_token: user_token})}
   end
 
   # PubSub Events
   def handle_info(:new_message, socket) do
-    messages = Message.get_all()
-    {:noreply, assign(socket, :messages, messages)}
+    send_update LiveChatComponent, id: "hero", user_token: socket.assigns.user_token
+    {:noreply, socket}
+  end
+
+  def handle_info({:flash, {severity, message}}, socket) do
+    {:noreply, put_flash(socket, severity, message)}
   end
 end
